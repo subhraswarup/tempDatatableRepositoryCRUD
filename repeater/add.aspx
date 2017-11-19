21963c95e4d56940a074f8c8236aae15606edcfc
subhraswarup <subhraswarup@outlook.com>
2017-11-19 18:32:57 +0530

    sdfs

---
+++ repeater/add.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="add.aspx.vb" Inherits="repeater.add" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:Repeater ID="Repeater1" runat="server">
+        <HeaderTemplate>
+        <table       >
+            
+    </HeaderTemplate>
+    <ItemTemplate>
+        <tr>
+             
+            <td>
+                <asp:Label ID="PostId" runat="server" Text='<%# Eval("PostId") %>' Visible="false" />
+                <asp:Label ID="lblPostEmail" runat="server" Text='<%# Eval("Email") %>' />
+                <asp:TextBox ID="txtPostEmail" runat="server" Width="120" Text='<%# Eval("Email") %>' Visible="false" />
+                <asp:HiddenField ID="hdnId" runat="server" Value='<%# Eval("Id") %>' />
+            </td>
+            <td>
+               <asp:button ID="btnEdit" Text="Edit" runat="server"  CommandName="edit" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "PostId") %>' />
+               <asp:button ID="btnUpdate" Text="Update" runat="server" Visible="false"    CommandName="update" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "PostId") %>' />
+               <asp:button ID="btnDelete" Text="Delete" runat="server" CommandName="delete"  OnClientClick="return confirm('Do you want to delete this row?');" />
+                
+            </td>
+        </tr>
+    </ItemTemplate>
+        
+         
+    </asp:Repeater>
+    <table   cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+        <tr>
+            <td style="width: 150px">
+                Email id:
+            </td>
+            <td style="width: 150px">
+                <asp:TextBox ID="txtEmail" runat="server" Width="140" />
+            </td>
+            <td style="width: 100px">
+                <asp:Button ID="btnAdd" runat="server" Text="Add"  />
+            </td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ repeater/add.aspx.vb
@@ -0,0 +1,88 @@
+﻿Public Class add
+    Inherits System.Web.UI.Page
+    Dim gObjDtReport As New DataTable
+    Dim rand As New Random
+    Private Sub BindRepeater()
+        Repeater1.DataSource = gObjDtReport
+        Repeater1.DataBind()
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If IsPostBack = False Then
+            CreateDatatable()
+            BindRepeater()
+        Else
+            gObjDtReport = CType(ViewState("DataTable"), DataTable)
+        End If
+        ViewState("DataTable") = gObjDtReport
+    End Sub
+    Private Sub CreateDatatable()
+        Try
+            gObjDtReport.Columns.Add("Id")
+            gObjDtReport.Columns.Add("PostId")
+            gObjDtReport.Columns.Add("brokerId")
+            gObjDtReport.Columns.Add("email")
+            gObjDtReport.Columns.Add("createdon")
+            gObjDtReport.Columns.Add("createdby")
+        Catch ex As Exception
+            Response.Write("<script>alert('" & ex.Message & "')</script>")
+        End Try
+    End Sub
+    Private Sub AddToDataTable()
+        Try
+            Dim strSts As String = 0
+            Dim dr As DataRow = gObjDtReport.NewRow()
+            dr("Id") = rand.Next(0, 1000)
+            dr("PostId") = "0"
+            dr("brokerId") = 35
+            dr("email") = txtEmail.Text
+            dr("createdon") = DateTime.Now.Date
+            dr("createdby") = 1
+            gObjDtReport.Rows.Add(dr)
+        Catch ex As Exception
+            Response.Write("<script>alert('" & ex.Message & "')</script>")
+        End Try
+    End Sub
+
+    Protected Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        AddToDataTable()
+        BindRepeater()
+    End Sub
+
+    Protected Sub Repeater1_ItemCommand(source As Object, e As RepeaterCommandEventArgs) Handles Repeater1.ItemCommand
+        Dim lblName As Label = e.Item.FindControl("lblPostEmail")
+        Dim lblrptID As Label = e.Item.FindControl("PostId")
+        Dim txtName As TextBox = e.Item.FindControl("txtPostEmail")
+        Dim btnEdit As Button = e.Item.FindControl("btnEdit")
+        Dim btnUpdate As Button = e.Item.FindControl("btnUpdate")
+        Dim btnDelete As Button = e.Item.FindControl("btnDelete")
+        Dim hdnId As HiddenField = e.Item.FindControl("hdnId")
+        If e.CommandName = "edit" Then
+            txtName.Visible = True
+            lblName.Visible = False
+            btnEdit.Visible = False
+            btnUpdate.Visible = True
+        End If
+        If e.CommandName = "update" Then
+            If lblrptID.Text = "0" Then
+                For Each row As DataRow In gObjDtReport.Rows
+                    Dim currnetId As Int32 = Convert.ToInt32(row("Id"))
+                    If Convert.ToInt32(hdnId.Value) = currnetId Then
+                        row("email") = txtName.Text
+                        lblName.Text = txtName.Text
+                    End If
+                Next
+            Else
+
+            End If
+            lblName.Visible = True
+                txtName.Visible = False
+                btnEdit.Visible = True
+                btnUpdate.Visible = False
+            End If
+        If e.CommandName = "delete" Then
+            gObjDtReport.Rows(e.Item.ItemIndex).Delete()
+        End If
+        'BindRepeater()
+    End Sub
+End Class
